121c56bde9ed0b8b6268cdc4cf89f6dd654ae5a0
Mikael Törnwall <mikael.tornwall@aalto.fi>
2020-02-17 16:41:46 +0200

    tehtavat 9-10

---
+++ viikko2/tehtavat9-10/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -0,0 +1,12 @@
+
+package ohtu.verkkokauppa;
+
+import java.util.ArrayList;
+
+public class Kirjanpito extends KirjanpitoRajapinta {
+
+    public Kirjanpito() {
+        tapahtumat = new ArrayList<String>();
+    }
+    
+}
